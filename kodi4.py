566a7e0e1eb5f6438627a33f5829b4e1935e0e24
oldchip <37394020+oldchip@users.noreply.github.com>
2021-01-26 01:27:05 +0700

    Update kodi4.py

--- kodi4.py
+++ kodi4.py
@@ -1153,7 +1153,7 @@ def GetFShareCred():
 def LoginOKNoti(user="",lvl=""):
 	header = "[COLOR red][B]CHÚC MỪNG NĂM MỚI![/B][/COLOR]"
 	message = "[COLOR red][B]HAPPY NEW YEAR ![/B][/COLOR]"
-	xbmc.executebuiltin('Notification([COLOR yellow]"{}", "{}", "{}", "")'.format(header, message, "20000"))
+	xbmc.executebuiltin('[COLOR yellow]Notification("{}", "{}", "{}", "")'.format(header, message, "20000"))
 
 
 def GetFShareUser(cred):
